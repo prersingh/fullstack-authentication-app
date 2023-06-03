3a72201bd3c598b7ccf083788758ddadfa9d6d37
preranasinghh <preranasingh1176@gmail.com>
2023-06-03 20:07:16 +0530

    adding homepage

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './HomePage.css';
 
 const HomePage = () => {
-  return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+  const [userData, setUserData] = useState(null);
 
-export default HomePage
+  useEffect(() => {
+    const userEmail = localStorage.getItem('userEmail');
+
+    axios
+      .get(`http://localhost:8081/user/${userEmail}`)
+      .then((response) => {
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching user data:', error);
+      });
+  }, []);
+
+  return (
+    <div className="success-page">
+      <div className="success-box">
+        {userData ? (
+          <>
+           <h2>Name: <span>{userData.name}</span></h2>
+      <h2>Email: <span>{userData.email}</span></h2>
+      <h2>Email status: <strong>Verified</strong></h2>
+          </>
+        ) : (
+          <p>Loading user data...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
